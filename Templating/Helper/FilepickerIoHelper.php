948a90d4303886a5039239075022bdab46c482a5
Sasha Lavasov <imsashko@gmail.com>
2013-01-21 23:59:31 +0200

    fix for input params list

--- Templating/Helper/FilepickerIoHelper.php
+++ Templating/Helper/FilepickerIoHelper.php
@@ -28,14 +28,14 @@ class FilepickerIoHelper extends Helper
     public function saveButton($text, $url, $mimetype, $options)
     {
         $inputPrams = array(
-            'data-fp-url' => ,
-            'data-fp-apikey' => ,
-            'data-fp-mimetype' => ,
-            'data-fp-extension' => ,
-            'data-fp-container' => ,
-            'data-fp-service' => ,
-            'data-fp-services' => ,
-            'data-fp-suggestedFilename' => ,
+            'data-fp-url' => '',
+            'data-fp-apikey' => '',
+            'data-fp-mimetype' => '',
+            'data-fp-extension' => '',
+            'data-fp-container' => '',
+            'data-fp-service' => '',
+            'data-fp-services' => '',
+            'data-fp-suggestedFilename' => '',
             );
         $tmplName = 'WebmilFilepickerIoBundle::save_button.html.twig';
 
